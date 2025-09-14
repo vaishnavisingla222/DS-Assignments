c4a88b9cde6f407d13113f0cab755565949b8dff
Vaishnavi Singla <vsingla_be24@thapar.edu>
2025-09-14 12:42:53 +0530

    Create Q3.cpp

---
+++ Assignment 1/Q3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int arr[5]={1};
+    for(i=0;i<5;i++)
+    cout<<arr[i]<<endl;
+
+    return 0;
+}
+/*Output is:
+   1
+   0
+   0
+   0
+   0
+*/
