8a8de0cb18f8d59185adcfcd980841fd87162b5a
Vaishnavi Singla <vsingla_be24@thapar.edu>
2025-09-14 12:22:23 +0530

    Update Q2.cpp

--- Assignment 3/Q2.cpp
+++ Assignment 3/Q2.cpp
@@ -4,24 +4,23 @@ using namespace std;
 
 int main()
 {
-    int top = 0;
+    int top=0;
     char stack[MaxLen];
     char str[MaxLen];
 
-    cout << "Enter the word without space: ";
+    cout <<"Enter the word without space: ";
     cin >> str;
 
-    for(int i = 0; str[i] != '\0'; i++) 
+    for(int i=0; str[i]!='\0';i++) 
     {
-        stack[top++] = str[i];
+        stack[top++]=str[i];
     }
     top--;
 
-    cout << "Reversed string: ";
-    while(top != -1) 
+    cout <<"Reversed string: ";
+    while(top!=-1) 
     {
-        cout << stack[top--];
+        cout<<stack[top--];
     }
-
     return 0;
 }
