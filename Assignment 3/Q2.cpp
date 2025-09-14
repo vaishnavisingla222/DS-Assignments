fd28c044758ae5c3a5f4da2c5f59729ab07420ce
Vaishnavi Singla <vsingla_be24@thapar.edu>
2025-09-14 12:06:34 +0530

    Create Q2.cpp

---
+++ Assignment 3/Q2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#define MaxLen 100
+
+int main()
+{
+    int top = 0;
+    char stack[MaxLen];
+    char str[MaxLen];
+
+    cout << "Enter the word without space: ";
+    cin >> str;
+
+    for(int i = 0; str[i] != '\0'; i++) 
+    {
+        stack[top++] = str[i];
+    }
+    top--;
+
+    cout << "Reversed string: ";
+    while(top != -1) 
+    {
+        cout << stack[top--];
+    }
+
+    return 0;
+}
