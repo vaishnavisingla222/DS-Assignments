28446ab0fcccdadcc9a420411fae8f1544ad7855
Vaishnavi Singla <vsingla_be24@thapar.edu>
2025-09-14 16:01:56 +0530

    Create Extra_4.cpp

---
+++ Assignment 2/Extra_4.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size;
+    cout<<"Enter the size of the array: ";
+    cin>>size;
+
+    int arr[size];
+    cout<<"Enter the ellements of array(0,1,2): ";
+    for(int i=0;i<size;i++)
+    cin>>arr[i];
+
+    int c0=0,c1=0,c2=0;
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i]==0)
+        c0++;
+
+        else if(arr[i]==1)
+        c1++;
+    
+        else
+        c2++;
+    }
+    int k=0;
+    for(int i=0;i<c0;i++)
+    arr[k++]=0;
+
+    for(int i=0;i<c1;i++)
+    arr[k++]=1;
+
+    for(int i=0;i<c2;i++)
+    arr[k++]=2;
+
+    cout<<"New array: ";
+    for(int i=0;i<size;i++)
+    cout<<arr[i]<<" ";
+    return 0;
+}
