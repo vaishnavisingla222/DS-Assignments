4161d8c5ef31c7cbde55d2e39e6ce7b8689d0065
Vaishnavi Singla <vsingla_be24@thapar.edu>
2025-11-18 21:05:15 +0530

    Add heap sort implementation in Q5.cpp
    Implement heap sort algorithm with increasing order option.

---
+++ Assignment 8/Q5.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[],int n,int i,bool increasing) 
+{
+    int extreme=i;
+    int left=2*i+1;
+    int right=2*i+2;
+
+    if (increasing) 
+    { 
+        if (left<n && arr[left]>arr[extreme])
+        extreme = left;
+      
+        if (right<n && arr[right]>arr[extreme])
+        extreme=right;
+    } 
+    else 
+    {
+        if (left<n && arr[left]<arr[extreme])
+        extreme=left;
+        if (right<n && arr[right]<arr[extreme])
+        extreme=right;
+    }
+
+    if (extreme != i) 
+    {
+        int temp=arr[i];
+        arr[i]=arr[extreme];
+        arr[extreme]=temp;
+        heapify(arr,n,extreme,increasing);
+    }
+}
+
+void heapSort(int arr[], int n, bool increasing) 
+{
+    for (int i=n/2-1;i>=0;i--)
+    heapify(arr, n, i, increasing);
+
+    
+    for (int i = n - 1; i > 0; i--) 
+    {    
+        int temp=arr[0];
+        arr[0]=arr[i];
+        arr[i]=temp;
+      
+        heapify(arr,i,0,increasing);
+    }
+}
+
+int main() 
+{
+    int arr[]={12, 11, 13, 5, 6, 7};
+    int n=6;
+
+    cout<<"Original array: ";
+    for (int i=0;i<n; i++) 
+    cout<<arr[i]<<" ";
+
+    bool increasing=true;
+    heapSort(arr,n,increasing);
+
+    cout<<"\nSorted array:";
+    for (int i=0;i<n;i++) 
+    cout<<arr[i]<<" ";
+  
+    cout<<endl;
+
+    return 0;
+}
